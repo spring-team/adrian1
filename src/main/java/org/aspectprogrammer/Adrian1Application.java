ccee3203ce2bf9afd4e565827bdc6e2e51904263
Rod Johnson <johnsonroda@gmail.com>
2018-03-06 16:01:02 +0000

    Initial commit from Atomist

---
+++ src/main/java/org/aspectprogrammer/Adrian1Application.java
@@ -0,0 +1,12 @@
+package org.aspectprogrammer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Adrian1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Adrian1Application.class, args);
+	}
+}
